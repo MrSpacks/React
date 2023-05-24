6f86c5163505ddc04fb5a62d36a84d27de337894
Sergei <mr.spacks@seznam.cz>
2023-05-24 19:31:48 +0200

    run project "react"

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/header/header.jsx'
+
+
+function App() {
+  return (
+    
+    <div className="App">
+      <Header />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
