29391cfdb1a5467d76485caae2162749f37b7af9
Sergei <mr.spacks@seznam.cz>
2023-06-01 20:05:23 +0200

    add header for index.html

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,6 @@ function App() {
     
     <div className="App">
       <Header />
-      <h1>Hello</h1>
     </div>
   );
 }
--- my-app/src/components/header/header.jsx
+++ my-app/src/components/header/header.jsx
@@ -1,9 +1,26 @@
 import './header.css'
+import logoImg from './../../img/icons/logo.svg'
 
 function Header () {
     return (
-        <header>
-            <h1>Hovna</h1>
+        <header className='header'>
+            <div className="conteiner">
+                <div className="header_row">
+                    <div className="header_logo">
+                        <img src={logoImg} alt="Logo" />
+                        <span>FASION</span>
+                    </div>
+                    <nav className="header_nav">
+                        <ul>
+                            <li><a href="#!">CATALOGUE</a></li>
+                            <li><a href="#!">FASHION</a></li>
+                            <li><a href="#!">FAVOURITE</a></li>
+                            <li><a href="#!">LIFESTYLE</a></li>
+                            <li><a href="#!" className='header_nav-btn'>SIGN UP</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
         </header>
     )
 }
