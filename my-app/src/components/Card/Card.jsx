eb2d3b89a399b774af0d90fdd2e91a135265fc11
Sergei <mr.spacks@seznam.cz>
2023-06-06 20:00:57 +0200

    ready card block

--- my-app/src/components/Card/Card.jsx
+++ my-app/src/components/Card/Card.jsx
@@ -1,8 +1,18 @@
 import './Card.css'
+import arrowImg from './../../img/icons/arrow.svg'
+const Card = ({title,img}) => {
 
-const Card = () => {
     return ( 
-        <div></div>
+        <div>
+            <img className='card_img' src={img} alt="" />
+            <div className="card_body">
+                <div className="card_text">
+                    <div className="card_title">{title}</div>
+                    <div className="card_subtitle">Explore Now!</div>
+                </div>
+                <div className="card_icon"><img src={arrowImg} alt="" /></div>
+            </div>
+        </div>
      );
 }
  
--- my-app/src/components/arrivals/arrivals.jsx
+++ my-app/src/components/arrivals/arrivals.jsx
@@ -1,5 +1,8 @@
 import './arrivals.css'
 import Card from './../Card/Card.jsx'
+import cat01Img from './../../img/categories/cat-01.jpg'
+import cat02Img from './../../img/categories/cat-02.jpg'
+import cat03Img from './../../img/categories/cat-03.jpg'
 
 const Arrivals = () => {
     return ( 
@@ -9,9 +12,9 @@ const Arrivals = () => {
             <h2 className="title_2">NEW ARRIVALS</h2>
         </div>
         <div className="arrivals_cards">
-            <div className="arrivals_card"><Card /></div>
-            <div className="arrivals_card"><Card /></div>
-            <div className="arrivals_card"><Card /></div>
+            <div className="arrivals_card"><Card title="Hoodies & Sweetshirt" img={cat01Img} /></div>
+            <div className="arrivals_card"><Card title="Coats & Parkas" img={cat02Img} /></div>
+            <div className="arrivals_card"><Card title="Tees & T-Shirt" img={cat03Img} /></div>
         </div>
         
     </div>
